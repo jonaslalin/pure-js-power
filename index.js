ef588c46520a80dc53d7a155a840b7469577af6b
Jonas Lalin <1ac6df24@gmail.com>
2019-08-02 10:20:55 +0200

    solution1

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function getBreadcrumbs(site, urlTree, maxSize = 10) {
-  // TODO: Add implementation
+  const augmentedUrlTree = [site, ...urlTree];
+  return Array.apply(null, Array(maxSize)).reduce(
+    (res, _, i) => ({
+      ...res,
+      [`depth${i + 1}`]: Array.apply(null, Array(i + 1))
+        .map((__, j) => augmentedUrlTree[j] || '')
+        .join('|')
+    }),
+    {}
+  );
 }
 
 module.exports = getBreadcrumbs;
